ef317199b193ce8df791dff239068f8132f1692e
jorrickhs <jorrickstempher@gmail.com>
2023-12-05 19:05:03 +0100

    Day4 fix

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,36 +1,41 @@
-private fun part1(inputList: List<String>) {
-    var result = 0
-    var worth = 1
-    var matches = 1
-    for (cards in inputList) {
-        val gameId = getCardId(cards)
-        val cardsNoIndex = cards.removeRange(0, 10)
-        val allNumbers = cardsNoIndex.split("\n")
-        allNumbers.forEach { nmr ->
-            var card = nmr.split(" | ")
-            card[0].split(" ").forEach { nummer ->
-                if (card[1].contains(nummer) && nummer != "") {
-                    if(matches < 3){
-                        result++
-                        matches++
-                    } else {
-                        result += worth*2
-                        worth++
+import kotlin.math.pow
+private fun part1(inputList: List<String>): Int {
+    return inputList.sumOf { card ->
+        val cardInfo = card.split(":").last().split('|')
+        val winningValues = cardInfo.first().trim().split(" ").mapNotNull { it.toIntOrNull() }
+        val ownedValues = cardInfo.last().trim().split(" ").mapNotNull { it.toIntOrNull() }
 
-                    }
-                }
-            }
-        }
-        worth = 1
-        matches = 1
+        val numberOfWinning = ownedValues.count { it in winningValues }
+
+        (2.0.pow((numberOfWinning - 1).toDouble())).toInt()
     }
-    println(result)
 }
+
+private fun part2(){
+
+}
+
 private fun getCardId(gameString: String): Int? {
     val regex = Regex("""Card\s+(\d+):""")
     val matchResult = regex.find(gameString)
     return matchResult?.groupValues?.get(1)?.toInt()
 }
+
 fun main() {
-    part1(fileToList("day4"))
+//    fun part10(input: List<String>): Int {
+//        return input.sumOf { line ->
+//            val ticketValues = line.split(":").last()
+//            val winningValues = ticketValues.split('|').first().split(" ").map { it.trim() }
+//                .mapNotNull { if (it.isNullOrEmpty()) null else it.toInt() }
+//            val actualValues = ticketValues.split('|').last().split(" ").map { it.trim() }
+//                .mapNotNull { if (it.isNullOrEmpty()) null else it.toInt() }
+//            val numberOfWinning = actualValues.sumOf {
+//                if (it in winningValues) 1L else 0L
+//            }
+//            2.0.pow((numberOfWinning - 1).toDouble()).toInt()
+//        }
+//    }
+//    println(part10(fileToList("day4")))
+    println(part1(fileToList("day4")))
+
 }
