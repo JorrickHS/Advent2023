065dfde7e50be0f9f62b71a9334dc8d84af5ba56
jorrickstempher <s1172166@student.windesheim.nl>
2023-12-05 18:03:56 +0100

    UpdateD4

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,7 +1,7 @@
 private fun part1(inputList: List<String>) {
     var result = 0
     var worth = 1
-    var matches = 0
+    var matches = 1
     for (cards in inputList) {
         val gameId = getCardId(cards)
         val cardsNoIndex = cards.removeRange(0, 10)
@@ -10,19 +10,21 @@ private fun part1(inputList: List<String>) {
             var card = nmr.split(" | ")
             card[0].split(" ").forEach { nummer ->
                 if (card[1].contains(nummer) && nummer != "") {
-                    matches++
-                    //1e match = 1 punt
-                    //2e match = 1 punt
-                    //3e match = 2 punt
-                    //4e match = 4 punt
-                    //5e match = 8 punt
-                    //etc...
+                    if(matches < 3){
+                        result++
+                        matches++
+                    } else {
+                        result += worth*2
+                        worth++
+
+                    }
                 }
             }
         }
         worth = 1
+        matches = 1
     }
-    println(matches)
+    println(result)
 }
 private fun getCardId(gameString: String): Int? {
     val regex = Regex("""Card\s+(\d+):""")
